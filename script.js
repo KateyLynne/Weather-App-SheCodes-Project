234327b20f4cf6e0410e2cc64cf8dfb1cb2e5b83
KateyLynne <katey.l.silvers@gmail.com>
2021-04-19 13:22:08 -0400

    Updated js link and temp functions

--- script.js
+++ script.js
@@ -29,16 +29,12 @@ function formatDate(timestamp) {
     "December",
   ];
   let month = months[now.getMonth()];
-
   let formattedDate = `${day}, ${month} ${date}, ${year} ${hour}:${minute}`;
-
   return formattedDate;
 }
-
 let now = new Date();
 let dateFunction = document.querySelector(".currentDate");
 dateFunction.innerHTML = formatDate(now);
-
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -52,56 +48,49 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  fahrenheitTemperature = response.data.main.temp;
 }
-
 function searchCity(city) {
   let apiKey = "24f2d5d36b4c40e473a9a9526b9d4c9d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   searchCity(city);
 }
-
 function userLocation(position) {
   let apiKey = "24f2d5d36b4c40e473a9a9526b9d4c9d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemperature);
 }
-
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(userLocation);
 }
-
 function convertToCelsius(event) {
   event.preventDefault();
   let currentTemperature = document.querySelector("#temperature");
-  currentTemperature.innerHTML = 0;
+  currentTemperature.innerHTML = Math.round(
+    ((fahrenheitTemperature - 32) * 5) / 9
+  );
 }
-
 function convertToFahrenheit(event) {
   event.preventDefault();
   let currentTemperature = document.querySelector("#temperature");
-  currentTemperature.innerHTML = 0;
+  currentTemperature.innerHTML = Math.round(fahrenheitTemperature);
 }
+let fahrenheitTemperature = null;
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
-
 let currentLocationButton = document.querySelector("#geolocation-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
 searchCity("Charlotte");
